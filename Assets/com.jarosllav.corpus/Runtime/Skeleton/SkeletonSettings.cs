8e4afc3163521253c5cdfa8069a376053ea07c0a
Paweł Olszak <pawelolszak.00@gmail.com>
2026-04-02 14:40:43 +0200

    chore: add SkeletonSettings class

---
+++ Assets/com.jarosllav.corpus/Runtime/Skeleton/SkeletonSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace Corpus.Skeleton
+{
+    [Serializable]
+    public class SkeletonSettings
+    {
+        [SerializeField]
+        private BoneReference[] _bones;
+        
+        public BoneReference[] Bones => _bones;
+    }
+}
