cf4141f4189ccbe8e9c3dd49b8f45d3e26b9f23a
Paweł Olszak <pawelolszak.00@gmail.com>
2026-04-03 11:50:24 +0200

    feat: Skeleton service

---
+++ Assets/com.jarosllav.corpus/Runtime/Skeleton/BoneDefinition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Corpus.Skeleton
+{
+    [CreateAssetMenu(menuName = "Corpus/Skeleton/Bone Definition")]
+    public class BoneDefinition : ScriptableObject
+    {
+        [SerializeField]
+        private int _id;
+        
+        [SerializeField]
+        private string _code = string.Empty;
+        
+        public int ID => _id;
+        public string Code => _code;
+    }
+}
---
+++ Assets/com.jarosllav.corpus/Runtime/Skeleton/BoneReference.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Corpus.Skeleton
+{
+    [Serializable]
+    public class BoneReference
+    {
+        [SerializeField]
+        private BoneDefinition _definition;
+            
+        [SerializeField]
+        private Transform _transform;
+
+        public BoneDefinition Definition => _definition;
+        public Transform Transform => _transform;
+    }
+}
