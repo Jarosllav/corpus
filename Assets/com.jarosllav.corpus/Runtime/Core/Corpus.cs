970b19b5afc42c9b9e2a7db13516cf6e35d138be
Paweł Olszak <pawelolszak.00@gmail.com>
2026-04-04 13:08:17 +0200

    chore: impl PhysicsService in Corpus class

--- Assets/com.jarosllav.corpus/Runtime/Core/Corpus.cs
+++ Assets/com.jarosllav.corpus/Runtime/Core/Corpus.cs
@@ -11,19 +11,27 @@ namespace Corpus.Core
         
         [SerializeField]
         private SkeletonSettings _skeletonSettings;
+        
+        [SerializeField]
+        private PhysicsSettings _physicsSettings;
 
 #if UNITY_EDITOR
         [SerializeField]
         private bool _drawSkeletonGizmos = true;
+        
+        [SerializeField]
+        private bool _drawPhysicsGizmos = true;
 #endif
         
         #endregion
         
         private SkeletonService _skeletonService;
+        private PhysicsService _physicsService;
         
         public void Awake()
         {
             _skeletonService = new(_skeletonSettings);
+            _physicsService = new(_physicsSettings, _skeletonService);
         }
 
         public void Start()
@@ -38,30 +46,41 @@ namespace Corpus.Core
 
         public void Update()
         {
-#if UNITY_EDITOR
-            if (!Application.isPlaying)
-            {
-                return;
-            }
-#endif
             var deltaTime = Time.deltaTime;
+            
         }
 
         public void FixedUpdate()
         {
             var deltaTime = Time.fixedDeltaTime;
+            
+            _physicsService.FixedTick(deltaTime);
         }
         
+
 #if UNITY_EDITOR
+
+        public void OnValidate()
+        {
+            if (Application.isPlaying)
+            {
+                return;
+            }
+            
+            _skeletonService = new(_skeletonSettings);
+            _physicsService = new(_physicsSettings, _skeletonService);
+        }
 
         public void OnDrawGizmos()
         {
             if (_drawSkeletonGizmos) SkeletonService.DrawGizmos(_skeletonService, _skeletonSettings);
+            if (_drawPhysicsGizmos) PhysicsService.DrawGizmos(_physicsService, _physicsSettings);
         }
 
         public void OnDrawGizmosSelected()
         {
             if (_drawSkeletonGizmos) SkeletonService.DrawGizmos(_skeletonService, _skeletonSettings, true);
+            if (_drawPhysicsGizmos) PhysicsService.DrawGizmos(_physicsService, _physicsSettings, true);
         }
         
 #endif
