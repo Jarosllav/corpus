bc4fc322987324b9b4526fb28b3efaf5e0deb35c
Paweł Olszak <pawelolszak.00@gmail.com>
2026-04-04 13:11:37 +0200

    chore: add MotorService class, add MotorSettings class

---
+++ Assets/com.jarosllav.corpus/Runtime/Motor/MotorService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using UnityEngine;
+using Corpus.Physics;
+
+namespace Corpus.Motor
+{
+    public class MotorService
+    {
+        private readonly MotorSettings _settings;
+        private readonly PhysicsService _physicsService;
+        
+        private Vector3 _inverseMotion = Vector3.zero;
+
+        public MotorService(MotorSettings settings, PhysicsService physicsService)
+        {
+            _settings = settings;
+            _physicsService = physicsService;
+        }
+
+        public void FixedTick(float deltaTime)
+        {
+            var displacement = _physicsService.Velocity * deltaTime;
+
+            Move(displacement);
+        }
+
+        public void Move(Vector2 direction)
+        {
+            var worldDirection = direction.x * _settings.Transform.right 
+                                 + direction.y * _settings.Transform.forward;
+            Move(worldDirection.normalized);
+        }
+        
+        public void Move(Vector3 worldDirection)
+        {
+            var motion = _settings.Speed * Time.deltaTime * worldDirection;
+
+            ApplyMotion(motion);
+            
+            _inverseMotion = _settings.Transform.InverseTransformDirection(worldDirection);
+        }
+
+        private void ApplyMotion(Vector3 displacement)
+        {
+            var position = _settings.Transform.position;
+            
+            if (_physicsService.ProbeCollisions(displacement))
+            {
+                
+            }
+
+            position += displacement;
+            
+            if (_physicsService.IsGrounded)
+            {
+                position.y = _physicsService.GroundLevel;
+            }
+            
+            _settings.Transform.position = position;
+        }
+    }
+}
---
+++ Assets/com.jarosllav.corpus/Runtime/Motor/MotorSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Corpus.Motor
+{
+    [Serializable]
+    public class MotorSettings
+    {
+        [SerializeField]
+        private Transform _transform;
+        
+        [SerializeField]
+        private float _speed = 10f;
+        
+        public Transform Transform => _transform;
+        public float Speed => _speed;
+    }
+}
